4f12586eae6386070d71a3b39133887000230ea8
CongTruongTran <trancongtruong240401@gmmail.com>
2022-04-25 21:54:05 +0700

    bai tap da hinh

---
+++ BTDaHinh/BTDaHinh/Hinh.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BTDaHinh
+{
+    class Hinh
+    {
+        public int socanh
+        {
+            get;
+            set;
+        }
+        public int sodinh
+        {
+            get;
+            set;
+        }
+        public HinhHoc(int Canh, int Dinh)
+        {
+            socanh = Canh;
+            sodinh = Dinh;
+            
+        }
+    }
+}
---
+++ BTDaHinh/BTDaHinh/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BTDaHinh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
