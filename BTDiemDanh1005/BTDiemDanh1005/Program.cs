1151f97dea46e06e34dd8b2039263aad26612b9b
CongTruongTran <trancongtruong240401@gmmail.com>
2022-05-10 07:44:59 +0700

    bai tap diem danh 1005

--- BTDiemDanh1005/BTDiemDanh1005/Program.cs
+++ BTDiemDanh1005/BTDiemDanh1005/Program.cs
@@ -7,12 +7,12 @@ namespace BTDiemDanh1005
         static void Main(string[] args)
         {
             int[] arr167 = new int[100];
-
+            int i,j;
             Console.Write("Nhap so phan tu trong mang: ");
             int n167 = int.Parse(Console.ReadLine());
 
             Console.Write("Nhap {0} phan tu vao trong mang:\n", n167);
-            for (int i = 0; i < n167; i++)
+            for (i = 0; i < n167; i++)
             {
                 Console.Write("Phan tu - {0}: ", i);
                 arr167[i] = int.Parse(Console.ReadLine());
@@ -22,7 +22,7 @@ namespace BTDiemDanh1005
             int min167 = arr167[0];
 
 
-            for (int i = 1; i < n167; i++)
+            for (i = 1; i < n167; i++)
             {
                 if (arr167[i] > max167)
                 {
@@ -35,8 +35,32 @@ namespace BTDiemDanh1005
                     min167 = arr167[i];
                 }
             }
+            
+            
             Console.WriteLine("Phan tu lon nhat trong mang la: {0}", max167);
             Console.WriteLine("Phan tu nho nhat trong mang la: {0}\n", min167);
+
+            int tmp167;
+            for (i = 0; i < n167; i++)
+            {
+                for (j = i + 1; j < n167; j++)
+                {
+                    if (arr167[j] < arr167[i])
+                    {
+                        //cach trao doi gia tri
+                        tmp167 = arr167[i];
+                        arr167[i] = arr167[j];
+                        arr167[j] = tmp167;
+                    }
+                }
+            }
+
+            Console.WriteLine("In cac phan tu mang theo thu tu tang dan:\n");
+            for (i = 0; i < n167; i++)
+            {
+                Console.Write("  {0}", arr167[i]);
+            }
+
 
             Console.ReadKey();
         }
